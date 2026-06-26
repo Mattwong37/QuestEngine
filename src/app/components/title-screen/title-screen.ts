1820f0caa8e1ff3a2c12138d19b9574d7e1bf2e0
Matt W <mattwong37@gmail.com>
2026-06-26 02:55:06 -0400

    feat: Add name input for character creation

--- src/app/components/title-screen/title-screen.ts
+++ src/app/components/title-screen/title-screen.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-title-screen',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './title-screen.html',
   styleUrl: './title-screen.css',
 })
-export class TitleScreen {}
+
+export class TitleScreen {
+  currentScreen = signal('title');
+
+  playerName = signal('');
+  startGame() {
+    this.currentScreen.set('character-creation');
+  } 
+
+  createCharacter() {
+    console.log('Player name:', this.playerName());
+  }
+}
