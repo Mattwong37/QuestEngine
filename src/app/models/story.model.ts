cd8fe8786c1c80595d379687a9aa3df4f90abbb0
Matt W <mattwong37@gmail.com>
2026-06-26 04:02:01 -0400

    feat: Added player model and configured it

---
+++ src/app/models/story.model.ts
@@ -0,0 +1,59 @@
+export interface Player {
+  name: string;
+  level: number;
+  xp: number;
+  xpToNextLevel: number;
+
+  maxHealth: number;
+  currentHealth: number;
+
+  maxMagic: number;
+  currentMagic: number;
+
+  defense: number;
+  magicDefense: number;
+
+  attackMin: number;
+  attackMax: number;
+
+  stamina: number;
+
+  equippedWeapon: Weapon | null;
+  inventory: Item[];
+  activeModifiers: StatModifier[];
+  choiceHistory: string[];
+}
+
+export interface StatModifier {
+  stat: string;
+  value: number;
+  turnsRemaining: number;
+}
+
+export interface Weapon {
+  name: string;
+  attackMin: number;
+  attackMax: number;
+}
+
+export interface Item {
+  id: string;
+  name: string;
+  description: string;
+  effect?: StatModifier;
+  permanent?: boolean;
+}
+
+export interface Choice {
+  id: string;
+  text: string;
+  nextNodeId: string;
+}
+
+export interface StoryNode {
+  id: string;
+  sceneText: string;
+  imagePrompt: string;
+  choices: Choice[];
+  isDialogue?: boolean;
+}
--- src/app/services/game.ts
+++ src/app/services/game.ts
@@ -1,8 +1,40 @@
 import { Injectable, signal } from '@angular/core';
+import { Player } from '../models/story.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
   playerName = signal('');
+
+  player = signal<Player>({
+    name: '',
+    level: 1,
+    xp: 0,
+    xpToNextLevel: 100,
+
+    maxHealth: 100,
+    currentHealth: 100,
+
+    maxMagic: 100,
+    currentMagic: 100,
+
+    defense: 0,
+    magicDefense: 0,
+
+    attackMin: 5,
+    attackMax: 5,
+
+    stamina: 100,
+
+    equippedWeapon: null,
+    inventory: [],
+    activeModifiers: [],
+    choiceHistory: [],
+  });
+
+  initPlayer(name: string) {
+    this.player.update(p => ({ ...p, name }));
+    this.playerName.set(name);
+  }
 }
