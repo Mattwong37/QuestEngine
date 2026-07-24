30edc8bb84a1a2e0d3eb2fb08b254fdc870cc0b6
Matt W <mattwong37@gmail.com>
2026-07-23 22:52:05 -0400

    test: Added boilerplate to title screen tests

--- src/app/components/title-screen/title-screen.spec.ts
+++ src/app/components/title-screen/title-screen.spec.ts
@@ -1,12 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TitleScreen } from './title-screen';
+import { ɵdevModeEqual } from '@angular/core';
 
 describe('TitleScreen', () => {
   let component: TitleScreen;
   let fixture: ComponentFixture<TitleScreen>;
 
   beforeEach(async () => {
+    localStorage.clear();
+    document.body.className = '';
+    vi.spyOn(window, 'alert');
+
     await TestBed.configureTestingModule({
       imports: [TitleScreen],
     }).compileComponents();
@@ -19,4 +24,25 @@ describe('TitleScreen', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('title screen navigation', () => {
+    it('startGame goes to character creation', () => {
+      component.startGame();
+      expect(component.currentScreen()).toBe('character-creation');
+    });
+  });
+
+  it('continueGame goes to game load', () => {
+    component.continueGame();
+    expect(component.currentScreen()).toBe('load');
+  });
+
+  //  TODO:
+  //  - Make sure all buttons work
+  //  - Make sure settings button hides when game starts
+  //  - test load save page
+  //  - test if text is right when no saves avaialble
+  //  - test character creation
+  //  - make sure settings works
+  //  - test dark mode toggle in title page setting
 });
