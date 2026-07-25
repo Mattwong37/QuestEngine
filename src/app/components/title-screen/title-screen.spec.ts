3a0ed34fb48fb43f6385499579d5d9fff66d8c0c
Matt W <mattwong37@gmail.com>
2026-07-25 15:42:48 -0400

    test: Added tests for navigation and button functionality

--- src/app/components/title-screen/title-screen.spec.ts
+++ src/app/components/title-screen/title-screen.spec.ts
@@ -1,11 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TitleScreen } from './title-screen';
-import { ɵdevModeEqual } from '@angular/core';
+import { GameService } from '../../services/game';
+
 
 describe('TitleScreen', () => {
   let component: TitleScreen;
   let fixture: ComponentFixture<TitleScreen>;
+  let gameService: GameService;
 
   beforeEach(async () => {
     localStorage.clear();
@@ -19,6 +21,7 @@ describe('TitleScreen', () => {
     fixture = TestBed.createComponent(TitleScreen);
     component = fixture.componentInstance;
     await fixture.whenStable();
+    gameService = component.gameService;  
   });
 
   it('should create', () => {
@@ -32,17 +35,44 @@ describe('TitleScreen', () => {
     });
   });
 
-  it('continueGame goes to game load', () => {
+  it('creating character goes to game page', () => {
+    component.playerName.set('Matt');
+    component.createCharacter();
+    expect(component.currentScreen()).toBe('game');
+  });
+
+  it('continueGame goes to save load page', () => {
     component.continueGame();
     expect(component.currentScreen()).toBe('load');
   });
 
-  //  TODO:
-  //  - Make sure all buttons work
-  //  - Make sure settings button hides when game starts
-  //  - test load save page
-  //  - test if text is right when no saves avaialble
-  //  - test character creation
-  //  - make sure settings works
-  //  - test dark mode toggle in title page setting
+  it('selecting a save goes to the game page', () => {
+    vi.spyOn(gameService, 'loadGame');
+    component.loadGame(1);
+    expect(component.currentScreen()).toBe('game');
+  });
+
+  it('settings button brings up pip', () => {
+    const settingsBtn = fixture.nativeElement.querySelector('.settings-button');
+    expect(settingsBtn).not.toBeNull();
+    settingsBtn!.click();
+    fixture.detectChanges();
+
+    expect(component.showSettings()).toBe(true);
+    expect(fixture.nativeElement.querySelector('.settings-overlay')).not.toBeNull();
+  });
+
+  it('close in settinsg closes the pip', () => {
+    component.showSettings.set(true);
+    fixture.detectChanges();
+
+    const closeBtn = fixture.nativeElement.querySelector('.close-button');
+    expect(closeBtn).not.toBeNull();
+    closeBtn!.click();
+    fixture.detectChanges();
+
+    expect(component.showSettings()).toBe(false);
+    expect(fixture.nativeElement.querySelector('.settings-overlay')).toBeNull();
+  });
+  
 });
