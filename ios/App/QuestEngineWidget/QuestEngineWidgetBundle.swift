add36eebb8ce61da3276598c05652739be8fcf43
Matt W <mattwong37@gmail.com>
2026-08-03 19:41:09 -0400

    build: Boilerplate widget files

---
+++ ios/App/QuestEngineWidget/QuestEngineWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  QuestEngineWidgetBundle.swift
+//  QuestEngineWidget
+//
+//  Created by Matt on 8/2/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct QuestEngineWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        QuestEngineWidget()
+        QuestEngineWidgetControl()
+    }
+}
