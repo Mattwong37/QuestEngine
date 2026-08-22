8232ac6811e23f36db8e388c355449ea4717e40f
Matt W <mattwong37@gmail.com>
2026-08-22 16:34:57 -0400

    test: Fix dark mode tests
    I'm quote confident that the testa re mainly breaking now because my vitest configs were either cleared or are being ignored

--- src/app/services/dark-mode.spec.ts
+++ src/app/services/dark-mode.spec.ts
@@ -1,9 +1,11 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, getTestBed } from '@angular/core/testing';
+import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { DarkMode } from './dark-mode';
 
 describe('DarkMode', () => {
   let service: DarkMode;
+  getTestBed().initTestEnvironment(BrowserTestingModule, platformBrowserTesting());
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
